bea733ea4ec37a7efb4403020332c559f7a2fc66
Aruzhan <almazdosbala17@mail.ru>
2023-06-27 20:29:37 +0600

    sctipt2

--- js/script.js
+++ js/script.js
@@ -3,12 +3,22 @@
 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
 'Сколько фильмов вы уже посмотрели?'
 
+
+
 2) Создать объект personalMovieDB и в него поместить такие свойства:
     - count - сюда передается ответ на первый вопрос
     - movies - в это свойство поместить пустой объект
     - actors - тоже поместить пустой объект
     - genres - сюда поместить пустой массив
     - privat - в это свойство поместить boolean(логическое) значение false
+
+    const personalMovieDB = {
+count: numberOfFilms,
+movies: {},
+actors: {},
+genres: [],
+privat: false
+    };
 
 3) Задайте пользователю по два раза вопросы:
     - 'Один из последних просмотренных фильмов?'
@@ -19,7 +29,35 @@
         'logan': '8.1'
     }
 
+
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', ''),
+            c = prompt('Один из последних просмотренных фильмов?', ''),
+            d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
 Проверить, чтобы все работало без ошибок в консоли */
 
-'use strict';
+"use strict";
 
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+  b = prompt("На сколько оцените его?", ""),
+  c = prompt("Один из последних просмотренных фильмов?", ""),
+  d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
