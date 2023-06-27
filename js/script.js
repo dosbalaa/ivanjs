cc53139a44d80307717ba6d88be224b97628d02c
Aruzhan <almazdosbala17@mail.ru>
2023-06-27 17:01:36 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//alert("hello");
+
+//const result = confirm("are you here");
+
+//const answer = prompt("are you 18?", "");
+
+console.log("arr" + " - object");
+console.log(4 + " - object");
